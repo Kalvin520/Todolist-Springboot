9ff05764c30240b1bd52ac837d83e2c5b6723b2e
Qing_qing <63888141+QingQing-520@users.noreply.github.com>
2022-07-23 00:55:15 +0800

    Add files via upload

---
+++ todolist/src/main/java/com/caili/todolist/service/TodoService.java
@@ -0,0 +1,16 @@
+package com.caili.todolist.service;
+
+import com.caili.todolist.dao.TodoDao;
+import com.caili.todolist.entity.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    @Autowired
+    TodoDao todoDao; // 取得Dao物件
+
+    public Iterable<Todo> getTodos () {
+        return todoDao.findAll();
+    }
+}
